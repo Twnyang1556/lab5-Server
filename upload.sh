66862e94a21c5bd56349da39cb14ce9a1f8a737a
yang1556 <yang1556@purdue.edu>
2020-04-17 13:54:58 -0400

    Commit at  1:54:58 PM

--- upload.sh
+++ upload.sh
@@ -4,4 +4,4 @@ msg=now+" commit"
 git checkout master >> .local.git.out || echo
 git add src/*.cc src/routes/*.cc include/*.hh Makefile >> .local.git.out  || echo
 git commit -a -m "Commit at $now"  >> .local.git.out || echo
-git push origin master
+git push origin master -f
